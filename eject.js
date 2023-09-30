fc25df7cf27d85d7cda16290e319ad332e9938c4
roger gomez <rgomez@jumpmath.es>
2023-09-30 12:08:01 +0200

    update eject

--- eject.js
+++ eject.js
@@ -1,16 +1,3 @@
 import eject from "rsc-module";
 
-eject({
-  sourceDir: "./node_modules",
-  destDir: ".",
-  codeFiles: ["src/**/*.{js,jsx,ts,tsx,mjs,es,es6}"],
-  dependenciesFilter: (set) => {
-    for (const e of set) {
-      if (e === "rsc-module") {
-        const newSet = new Set();
-        newSet.add(e);
-        return newSet;
-      }
-    }
-  },
-});
+eject();
