057774f3617bbb6d6e2985dde308a9d6938bd0dd
roger gomez <rgomez@jumpmath.es>
2023-10-07 22:28:06 +0200

    use rsc-module 4.0

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,9 @@
-import { runApp } from "rsc-module/server";
+import { getApp } from "rsc-module/server";
 import Router from "./components/router.js";
 import HTML from "./html.js";
 
-runApp(Router, HTML);
+const app = getApp(Router, HTML);
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
